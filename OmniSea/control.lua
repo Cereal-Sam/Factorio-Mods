794d91eedf6e9335060712d673388a37656bada0
Cereal Sam <Samuel2507@users.noreply.github.com>
2020-09-15 13:07:50 +0200

    rewrite control.lua, balanced starter items

--- OmniSea/control.lua
+++ OmniSea/control.lua
@@ -1,24 +1,27 @@
-local landfill = 'landfill'
-local function init()
-	if game.item_prototypes['landfill-sand-3'] then
-		landfill = 'landfill-sand-3'
+script.on_init(function(event)
+	if remote.interfaces["freeplay"] then
+		local items_to_insert = remote.call("freeplay", "get_created_items")
+		local landfill = "landfill"
+		if game.item_prototypes["landfill-sand-3"] then
+			landfill = "landfill-sand-3"
+		end
+		items_to_insert["omnidensator-1"] = (items_to_insert["omnidensator-1"] or 0) + 1
+		items_to_insert["burner-omni-furnace"] = (items_to_insert["burner-omni-furnace"] or 0) + 1
+		items_to_insert["crystallizer"] = (items_to_insert["crystallizer"] or 0) + 1
+		items_to_insert[landfill] = (items_to_insert[landfill] or 0) + 200
+		--Nil the burner mining drill that omnimatter inserts
+		items_to_insert["burner-mining-drill"] = nil
+		--Start with 2 electric omnitractors to avoid unneeded handcrafting cellulose for fuel
+		items_to_insert["omnitractor-1"] = 2
+		items_to_insert["burner-omnitractor"] = nil
+		--Swap burner phlog into electric
+		items_to_insert["omniphlog-1"] = 1
+		items_to_insert["burner-omniphlog"] = nil
+		remote.call("freeplay", "set_created_items", items_to_insert)
 	end
-end
-script.on_init(init)	
-	
---More stuff to start with
-
-script.on_event(defines.events.on_player_created, function(event)
-	local player = game.players[event.player_index]
-	player.insert{name="omnidensator-1", count=1}
-	player.insert{name=landfill, count=150}
-	player.insert{name="wind-turbine-2", count=10}
---	player.insert{name="omnitractor-1", count=1}
 end)
 
-
 --Unlock startup1 with Omnite ( and 3+4 if omnimatter_energy is active)
-
 function checkCraftResultTechs(player,inventory)
     local inv = player.get_inventory(inventory)
     if inv.get_item_count("omnite") > 0 then
@@ -41,56 +44,4 @@ end
 script.on_event(defines.events.on_player_main_inventory_changed, function(event)
   local player = game.players[event.player_index]
   checkCraftResultTechs(player,defines.inventory.character_main)
-end)
---script.on_event(defines.events.on_player_quickbar_inventory_changed, function(event)
---  local player = game.players[event.player_index]
---  checkCraftResultTechs(player,defines.inventory.player_quickbar)
---end)
-
-
---Edit rock-chest
-
-function adjustContent(chest)
-	local inventory = chest.get_inventory(defines.inventory.chest)
-	inventory.clear()
-	log("hey")
-	local stuff = {
-		{landfill, 1000},
-		{"stone", 50},
-		{"small-electric-pole", 50},
-		{"small-lamp", 12},
-		{"iron-plate", 1200},
-		{"basic-circuit-board", 200},
-		{"stone-pipe", 100},
-		{"stone-pipe-to-ground", 50},
-		{"stone-brick", 500},
-		{"pipe", 27},
-		{"copper-pipe", 5},
-		{"iron-gear-wheel", 25},
-		{"iron-stick", 96},
-		{"pipe-to-ground", 2},
-		{"electronic-circuit", 10},
-		{"wind-turbine-2", 40},
---		{"omnidensator-1", 1}, --already in inventory
-		{"crystallizer", 1},
-		{"omnitractor-1", 1},
-		{"burner-omni-furnace", 1}
-
-	}
-	for _,v in ipairs(stuff) do
-		chest.insert{name = v[1], count = v[2]}
-	end
-end
-
-script.on_nth_tick(5,function(conf)
-	if not global.fixedSeablockOmni and game.tick > 0 then
-		for _,surface in pairs(game.surfaces) do
-			for _,force in pairs(game.forces) do
-				for _,entity in pairs(surface.find_entities_filtered{force=force,name="rock-chest"}) do
-					adjustContent(entity)
-				end
-			end
-		end
-		global.fixedSeablockOmni=true
-	end
 end)
